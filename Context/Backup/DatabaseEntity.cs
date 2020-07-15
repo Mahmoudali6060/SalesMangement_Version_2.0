6c0150810780bdd868ce1c1850e1f5a7f0ddbf60
ITZoneWebTeam <web@itzoneglobal.com>
2020-07-15 08:54:45 +0200

    Adding of Back Database helper

---
+++ Context/Backup/DatabaseEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Database.Backup
+{
+   public class DatabaseEntity
+    {
+        public string DatabaseName { get; set; }
+        public string FilePath { get; set; }
+        public string ConnectionString { get; set; }
+    }
+}
