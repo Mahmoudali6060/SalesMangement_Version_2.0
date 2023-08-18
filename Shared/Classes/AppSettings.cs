0f3c5a338aeb46cb64e5fa0e7f9b9d27a4548817
Mahmoud Salman <m.salman@aliaict.com>
2023-08-18 15:34:43 +0300

    Change Commission Rate

--- Shared/Classes/AppSettings.cs
+++ Shared/Classes/AppSettings.cs
@@ -7,7 +7,7 @@ namespace Shared.Classes
     public class AppSettings
     {
 
-        public const decimal CommissionRate = 0.09M;
+        public const decimal CommissionRate = 0.10M;
         public const decimal GiftRate = 1M;
         public const decimal Decentate = 1.5M;
         public const decimal ByaaRate = 4M;
